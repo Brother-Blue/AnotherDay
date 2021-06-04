3a4f3364f5418151fc3ce699f987f3a6c9443a61
Christian O'Neill <gusonech@student.gu.se>
2021-06-04 16:39:03 +0200

    Add login providers
    The _providers variable stores different login methods used by FirebaseAuth

--- app/src/main/java/com/plannd/anotherday/MainActivity.kt
+++ app/src/main/java/com/plannd/anotherday/MainActivity.kt
@@ -2,8 +2,16 @@ package com.plannd.anotherday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
 
 class MainActivity : AppCompatActivity() {
+
+    // Login methods
+    private val _providers = arrayListOf(
+        AuthUI.IdpConfig.GoogleBuilder().build(),
+        AuthUI.IdpConfig.EmailBuilder().build()
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
