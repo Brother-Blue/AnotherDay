12c9bb525f4603420f8ce4fd17cb52c731eb0967
Christian O'Neill <gusonech@student.gu.se>
2021-06-04 17:04:00 +0200

    Add sign-in feature
    Adds callback functionality when user returns from the login UI.
    
    On success the user is saved, on failure the error is logged and the user informed.

--- app/src/main/java/com/plannd/anotherday/MainActivity.kt
+++ app/src/main/java/com/plannd/anotherday/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.plannd.anotherday
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -10,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     // Request codes
     companion object {
+        const val TAG: String = "Another Day"
         private const val RC_SIGN_IN = 500
     }
 
@@ -39,6 +46,26 @@ class MainActivity : AppCompatActivity() {
                 RC_SIGN_IN)
         } else {
             // TODO: ("The user is signed in here")
+        }
+    }
+
+    // Called when starting an activity for a result
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            RC_SIGN_IN -> {
+                // Get the response from sign-in attempt
+                val response = IdpResponse.fromResultIntent(data)
+                if (resultCode == Activity.RESULT_OK) {
+                    // If sign-in successful, assign the user
+                    _user = FirebaseAuth.getInstance().currentUser
+                } else {
+                    // Log the error and inform the user
+                    Log.e(TAG, response?.error?.errorCode.toString())
+                    Toast.makeText(this, "Sign-in failed", Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
