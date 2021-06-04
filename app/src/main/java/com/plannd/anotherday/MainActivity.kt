77ab033f7881bae8c2e8b50722fb4ce43a2069d1
Christian O'Neill <gusonech@student.gu.se>
2021-06-04 16:52:21 +0200

    Start user auth
    Check for saved sign ins, otherwise starts the sign in activity

--- app/src/main/java/com/plannd/anotherday/MainActivity.kt
+++ app/src/main/java/com/plannd/anotherday/MainActivity.kt
@@ -3,8 +3,18 @@ package com.plannd.anotherday
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
+
+    // Request codes
+    companion object {
+        private const val RC_SIGN_IN = 500
+    }
+
+    // The logged in user
+    private var _user: FirebaseUser? = null
 
     // Login methods
     private val _providers = arrayListOf(
@@ -12,8 +22,23 @@ class MainActivity : AppCompatActivity() {
         AuthUI.IdpConfig.EmailBuilder().build()
     )
 
+    // Called when activity is created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Check if a user has a saved login
+        _user = FirebaseAuth.getInstance().currentUser
+        // If the user is not logged in startup the login activity
+        if (_user == null) {
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(_providers)
+                    .build(),
+                RC_SIGN_IN)
+        } else {
+            // TODO: ("The user is signed in here")
+        }
     }
 }
