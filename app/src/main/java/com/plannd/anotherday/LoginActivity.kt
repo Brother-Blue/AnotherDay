99bca09232ea8943a4b6bc221124f4507891d58d
Christian O'Neill <gusonech@student.gu.se>
2021-06-05 07:33:03 +0200

    Fix app theme and Google signin
    Broken night theme fixed
    Removed google icon svg
    Replaces custom button with google signin button

--- app/src/main/java/com/plannd/anotherday/LoginActivity.kt
+++ app/src/main/java/com/plannd/anotherday/LoginActivity.kt
@@ -2,10 +2,26 @@ package com.plannd.anotherday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.common.SignInButton
 
 class LoginActivity : AppCompatActivity() {
+
+    // ---- VIEWS ----
+    // Buttons
+    private lateinit var _btnGoogleSignIn: SignInButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Init views on activity created
+        _btnGoogleSignIn = findViewById(R.id.btnGoogleSignIn)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // View settings that can't be explicitly defined in XML
+        _btnGoogleSignIn.setSize(SignInButton.SIZE_STANDARD)
     }
 }
