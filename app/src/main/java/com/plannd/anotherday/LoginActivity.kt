8111c9183e07a3a1aeff21d86e2f85f169ece2a0
Christian O'Neill <gusonech@student.gu.se>
2021-06-05 10:03:38 +0200

    Update google login and auth
    Updated to use Firebase auth (not the UI)
    Allows for multiple authentication methods with Firebase
    Added temporary sign out button to Main activity
    Refactored Login activity code

--- app/src/main/java/com/plannd/anotherday/LoginActivity.kt
+++ app/src/main/java/com/plannd/anotherday/LoginActivity.kt
@@ -1,20 +1,36 @@
 package com.plannd.anotherday
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    // Google sign in vars
+    companion object {
+        private const val TAG = "Plannd"
+        // Codes
+        private const val RC_SIGN_IN = 1000
+    }
+
+    // ---- Auth vars ----
+    // Google
     private lateinit var _gso: GoogleSignInOptions // Sign in options
     private lateinit var _googleSignInClient: GoogleSignInClient // Sign in client
-    private var _googleAccount: GoogleSignInAccount? = null // Signed in account
-    // Signed in account
+    // Firebase
+    private lateinit var _firebaseAuth: FirebaseAuth // Firebase authentication
+    private var _user: FirebaseUser? = null
 
     // ---- VIEWS ----
     // Buttons
@@ -24,26 +40,82 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // Init firebase auth
+        _firebaseAuth = Firebase.auth
+
+        // Before instantiating anything, check if a user login is saved
+        _user = _firebaseAuth.currentUser
+        if (_user != null) {
+            updateUI(_user!!)
+        }
+
         // Setup the sign in options
         _gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
-        // Build sign in client with options
         _googleSignInClient = GoogleSignIn.getClient(this, _gso)
 
         // Init views on activity created
         _btnGoogleSignIn = findViewById(R.id.btnGoogleSignIn)
 
-
+        // Set listeners
+        _btnGoogleSignIn.setOnClickListener {
+            signInWithGoogle()
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-        // Get last signed in account, null if new user
-        _googleAccount = GoogleSignIn.getLastSignedInAccount(this)
+        when (requestCode) {
+            RC_SIGN_IN -> {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                try {
+                    // Get the account or throw and exception
+                    val account = task.getResult(ApiException::class.java)!!
+                    // Passed this point, the account is non-null
+                    Log.d(TAG, "Firebase auth (Google): " + account.id)
+                    // Authenticate the account
+                    firebaseAuthGoogleAccount(account.idToken!!)
+                } catch (e: ApiException) {
+                    Log.w(TAG, "Google sign in failed")
+                    Toast.makeText(this, "Google sign in failed.", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
 
-        // View settings that can't be explicitly defined in XML
-        _btnGoogleSignIn.setSize(SignInButton.SIZE_STANDARD)
+    // Update the UI, if the user is not null, move to main activity
+    // And finish the login activity
+    private fun updateUI(user: FirebaseUser) {
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    // Google sign in button
+    private fun signInWithGoogle() {
+        val intent = _googleSignInClient.signInIntent
+        startActivityForResult(intent, RC_SIGN_IN)
+    }
+
+    // Authenticate Google account with Firebase
+    private fun firebaseAuthGoogleAccount(accountToken: String) {
+        // Get account credentials using Firebase account token
+        val credential = GoogleAuthProvider.getCredential(accountToken, null)
+        _firebaseAuth.signInWithCredential(credential)
+            // On completion
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Firebase auth credentials success (Google)")
+                    _user = _firebaseAuth.currentUser
+                    updateUI(_user!!)
+                } else {
+                    Log.w(TAG, "Firebase auth credentials failure (Google)")
+                    Toast.makeText(this, "Google authentication in failed", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/plannd/anotherday/MainActivity.kt
+++ app/src/main/java/com/plannd/anotherday/MainActivity.kt
@@ -1,26 +1,43 @@
 package com.plannd.anotherday
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-    // Request codes
-    companion object {
-        const val TAG: String = "Another Day"
-    }
+    // Authenticator and user
+    private lateinit var _firebaseAuth: FirebaseAuth
 
-    // The logged in user
-    private var _user: FirebaseUser? = null
+    // ---- VIEWS ----
+    // Buttons
+    private lateinit var _btnSignOut: Button
 
     // Called when activity is created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Get the signed in user
-        _user = FirebaseAuth.getInstance().currentUser
+        // Init Firebase auth
+        _firebaseAuth = Firebase.auth
+
+        // Init views
+        _btnSignOut = findViewById(R.id.btnSignOut)
+
+        // Set listeners
+        _btnSignOut.setOnClickListener {
+            signOut()
+        }
+    }
+
+    // Signed out of account
+    private fun signOut() {
+        _firebaseAuth.signOut()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
